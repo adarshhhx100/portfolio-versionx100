806c4c7e4a691ab4ee0909d5eb14799cc6825b4d
extremistLab <adarsh.chb@gmail.com>
2024-07-21 00:08:35 +0530

    version1 commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+// src/components/Footer.tsx
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gradient-to-r from-blue-700 to-blue-900 text-white text-center p-4  bottom-0 w-full">
+      <p>&copy; This Portfolio Hancrafted by : Sir Adarsh Chaturvedi in 2024</p>
+    </footer>
+  );
+};
+
+export default Footer;
